91d84019bac71429bc9a3b39f9e0fe292f28ef97
tjquillan <tjquillan@gmail.com>
2021-05-29 23:42:33 -0700

    Port TruthTable parser over to latex
    Previously it used ascii-math, changing this allows the removal of the
    asciimath2tex dependancy.
    
    Additionally, added a input label and fixed up Typographys.

--- src/util/parser/proposition/grammar.ne
+++ src/util/parser/proposition/grammar.ne
@@ -1,20 +1,41 @@
 @preprocessor typescript
-@{%// eslint-disable-next-line import/first
-import {Atom, Paren, Not, And, Or, Xor, Implies, Iff} from './ast' %}
 
-proposition -> iff                  {% id %}
-iff         -> iff "↔" implies      {% (d) => new Iff(d[1], d[0], d[2]) %}      # IFF
-                | implies           {% id %}
-implies     -> implies "→" xor      {% (d) => new Implies(d[1], d[0], d[2]) %}  # Implies (if)
-                | xor               {% id %}
-xor         -> xor "⊕" or           {% (d) => new Xor(d[1], d[0], d[2]) %}      # Exclusive Or
-                | or                {% id %}
-or          -> or "∨" and           {% (d) => new Or(d[1], d[0], d[2]) %}       # Inclusive Or
-                | and               {% id %}
-and         -> and "∧" not          {% (d) => new And(d[1], d[0], d[2]) %}      # And
-                | not               {% id %}
-not         -> "¬" not              {% (d) => new Not(d[0], d[1], null) %}      # Negation
-                | paren             {% id %}
-paren       -> "(" proposition ")"  {% (d) => new Paren(null, d[1], null) %}    # Handle parentheses
-                | var               {% id %}
-var         -> [a-zA-Z]             {% (d) => new Atom(null, d[0], null) %}     # Var
+@{%
+// eslint-disable-next-line import/first
+import moo from 'moo'
+// eslint-disable-next-line import/first
+import {Atom, Paren, Not, And, Or, Xor, Implies, Iff} from './ast'
+%}
+
+@{%
+const lexer = moo.compile({
+  iff:      /\\leftrightarrow\b\s?/,
+  implies:  /\\to\b\s?/,
+  xor:      /\\oplus\b\s?/,
+  or:       /\\vee\b\s?/,
+  and:      /\\wedge\b\s?/,
+  not:      /\\neg\b\s?/,
+  lParen:   /\(\s?/,
+  rParen:   /\)\s?/,
+  symbol:   /[a-zA-Z]/
+})
+%}
+
+@lexer lexer
+
+proposition -> iff                          {% id %}
+iff         -> iff %iff implies             {% (d) => new Iff(d[1], d[0], d[2]) %}        # IFF
+             | implies                      {% id %}
+implies     -> implies %implies xor         {% (d) => new Implies(d[1], d[0], d[2]) %}    # Implies (if)
+             | xor                          {% id %}
+xor         -> xor %xor or                  {% (d) => new Xor(d[1], d[0], d[2]) %}        # Exclusive Or
+             | or                           {% id %}
+or          -> or %or and                   {% (d) => new Or(d[1], d[0], d[2]) %}         # Inclusive Or
+             | and                          {% id %}
+and         -> and %and not                 {% (d) => new And(d[1], d[0], d[2]) %}        # And
+             | not                          {% id %}
+not         -> %not not                     {% (d) => new Not(d[0], d[1], null) %}        # Negation
+             | paren                        {% id %}
+paren       -> %lParen proposition %rParen  {% (d) => new Paren(null, d[1], null) %}      # Handle parentheses
+             | atom                         {% id %}
+atom        -> %symbol                      {% (d) => new Atom(null, d[0].value, null) %} # Var
--- src/util/parser/proposition/grammar.ts
+++ src/util/parser/proposition/grammar.ts
@@ -2,67 +2,83 @@
 // http://github.com/Hardmath123/nearley
 // Bypasses TS6133. Allow declared but unused functions.
 // @ts-ignore
-function id(d: any[]): any {
-  return d[0]
-}
+function id(d: any[]): any { return d[0]; }
+declare var iff: any;
+declare var implies: any;
+declare var xor: any;
+declare var or: any;
+declare var and: any;
+declare var not: any;
+declare var lParen: any;
+declare var rParen: any;
+declare var symbol: any;
+
 // eslint-disable-next-line import/first
-import { Atom, Paren, Not, And, Or, Xor, Implies, Iff } from "./ast"
+import moo from 'moo'
+// eslint-disable-next-line import/first
+import {Atom, Paren, Not, And, Or, Xor, Implies, Iff} from './ast'
+
+
+const lexer = moo.compile({
+  iff:      /\\leftrightarrow\b\s?/,
+  implies:  /\\to\b\s?/,
+  xor:      /\\oplus\b\s?/,
+  or:       /\\vee\b\s?/,
+  and:      /\\wedge\b\s?/,
+  not:      /\\neg\b\s?/,
+  lParen:   /\(\s?/,
+  rParen:   /\)\s?/,
+  symbol:   /[a-zA-Z]/
+})
+
 interface NearleyToken {
-  value: any
-  [key: string]: any
-}
+  value: any;
+  [key: string]: any;
+};
 
 interface NearleyLexer {
-  reset: (chunk: string, info: any) => void
-  next: () => NearleyToken | undefined
-  save: () => any
-  formatError: (token: never) => string
-  has: (tokenType: string) => boolean
-}
+  reset: (chunk: string, info: any) => void;
+  next: () => NearleyToken | undefined;
+  save: () => any;
+  formatError: (token: never) => string;
+  has: (tokenType: string) => boolean;
+};
 
 interface NearleyRule {
-  name: string
-  symbols: NearleySymbol[]
-  postprocess?: (d: any[], loc?: number, reject?: {}) => any
-}
+  name: string;
+  symbols: NearleySymbol[];
+  postprocess?: (d: any[], loc?: number, reject?: {}) => any;
+};
 
-type NearleySymbol = string | { literal: any } | { test: (token: any) => boolean }
+type NearleySymbol = string | { literal: any } | { test: (token: any) => boolean };
 
 interface Grammar {
-  Lexer: NearleyLexer | undefined
-  ParserRules: NearleyRule[]
-  ParserStart: string
-}
+  Lexer: NearleyLexer | undefined;
+  ParserRules: NearleyRule[];
+  ParserStart: string;
+};
 
 const grammar: Grammar = {
-  Lexer: undefined,
+  Lexer: lexer,
   ParserRules: [
-    { name: "proposition", symbols: ["iff"], postprocess: id },
-    { name: "iff", symbols: ["iff", { literal: "↔" }, "implies"], postprocess: (d) => new Iff(d[1], d[0], d[2]) },
-    { name: "iff", symbols: ["implies"], postprocess: id },
-    {
-      name: "implies",
-      symbols: ["implies", { literal: "→" }, "xor"],
-      postprocess: (d) => new Implies(d[1], d[0], d[2])
-    },
-    { name: "implies", symbols: ["xor"], postprocess: id },
-    { name: "xor", symbols: ["xor", { literal: "⊕" }, "or"], postprocess: (d) => new Xor(d[1], d[0], d[2]) },
-    { name: "xor", symbols: ["or"], postprocess: id },
-    { name: "or", symbols: ["or", { literal: "∨" }, "and"], postprocess: (d) => new Or(d[1], d[0], d[2]) },
-    { name: "or", symbols: ["and"], postprocess: id },
-    { name: "and", symbols: ["and", { literal: "∧" }, "not"], postprocess: (d) => new And(d[1], d[0], d[2]) },
-    { name: "and", symbols: ["not"], postprocess: id },
-    { name: "not", symbols: [{ literal: "¬" }, "not"], postprocess: (d) => new Not(d[0], d[1], null) },
-    { name: "not", symbols: ["paren"], postprocess: id },
-    {
-      name: "paren",
-      symbols: [{ literal: "(" }, "proposition", { literal: ")" }],
-      postprocess: (d) => new Paren(null, d[1], null)
-    },
-    { name: "paren", symbols: ["var"], postprocess: id },
-    { name: "var", symbols: [/[a-zA-Z]/], postprocess: (d) => new Atom(null, d[0], null) }
+    {"name": "proposition", "symbols": ["iff"], "postprocess": id},
+    {"name": "iff", "symbols": ["iff", (lexer.has("iff") ? {type: "iff"} : iff), "implies"], "postprocess": (d) => new Iff(d[1], d[0], d[2])},
+    {"name": "iff", "symbols": ["implies"], "postprocess": id},
+    {"name": "implies", "symbols": ["implies", (lexer.has("implies") ? {type: "implies"} : implies), "xor"], "postprocess": (d) => new Implies(d[1], d[0], d[2])},
+    {"name": "implies", "symbols": ["xor"], "postprocess": id},
+    {"name": "xor", "symbols": ["xor", (lexer.has("xor") ? {type: "xor"} : xor), "or"], "postprocess": (d) => new Xor(d[1], d[0], d[2])},
+    {"name": "xor", "symbols": ["or"], "postprocess": id},
+    {"name": "or", "symbols": ["or", (lexer.has("or") ? {type: "or"} : or), "and"], "postprocess": (d) => new Or(d[1], d[0], d[2])},
+    {"name": "or", "symbols": ["and"], "postprocess": id},
+    {"name": "and", "symbols": ["and", (lexer.has("and") ? {type: "and"} : and), "not"], "postprocess": (d) => new And(d[1], d[0], d[2])},
+    {"name": "and", "symbols": ["not"], "postprocess": id},
+    {"name": "not", "symbols": [(lexer.has("not") ? {type: "not"} : not), "not"], "postprocess": (d) => new Not(d[0], d[1], null)},
+    {"name": "not", "symbols": ["paren"], "postprocess": id},
+    {"name": "paren", "symbols": [(lexer.has("lParen") ? {type: "lParen"} : lParen), "proposition", (lexer.has("rParen") ? {type: "rParen"} : rParen)], "postprocess": (d) => new Paren(null, d[1], null)},
+    {"name": "paren", "symbols": ["atom"], "postprocess": id},
+    {"name": "atom", "symbols": [(lexer.has("symbol") ? {type: "symbol"} : symbol)], "postprocess": (d) => new Atom(null, d[0].value, null)}
   ],
-  ParserStart: "proposition"
-}
+  ParserStart: "proposition",
+};
 
-export default grammar
+export default grammar;
